26b0e5b6710b2ce1b833f018c3f38363692aa150
Glitch (sneaky-octave) <none>
2019-01-07 15:48:54 +0000

    🌇🤑 Checkpoint ./routes/api.js:2521121/1300

--- routes/api.js
+++ routes/api.js
@@ -15,17 +15,39 @@ const MONGODB_CONNECTION_STRING = process.env.DB;
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DB, {useNewUrlParser: true});
 
+const bookSchema = mongoose.Schema({
+  title: {type: String, required: true},
+  comments: {type: [], required: false},
+  comment
+})
+
+const Book = mongoose.model('Book', bookSchema);
+
 module.exports = function (app) {
   
   app.route('/api/books')
     .get(function (req, res){
-      //response will be array of book objects
-      //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+      var query = req.body._id || {};
+      Book.find(query,(err,books)=>{
+        if (err) return res.json(err);
+        res.json(books);
+      })
     })
     
     .post(function (req, res){
       var title = req.body.title;
-      //response will contain new book object including atleast _id and title
+      Book.findOne({title: title}, (err,book)=>{
+        if (err) return res.json(err)
+        if (book) return res.json({error: "This title already exists!"})
+        var newBook = new Book({title: title},(err,data)=>{
+          if (err) return res.json(err)
+          newBook.save((err,data)=>{
+          if (err) return res.json(err)
+            res.json({title: data.title, _id: data._id});
+          })
+        })
+        
+      })
     })
     
     .delete(function(req, res){
